f676e22e5f5978ad8cadd1fb95b480d7f57e1269
Vishnuallapra <93085896+Vishnuallapra@users.noreply.github.com>
2021-11-23 22:28:52 +0530

    Updated

--- C_programs/prg17.c
+++ C_programs/prg17.c
@@ -1,45 +1,26 @@
-#include<stdio.h>
-void main()
-{
-int n,i,a=0,b=0,num;
-printf("Enter the limit");
-scanf("%d",&n);
-printf("Enter the numbers");
-for(i=0;i<n;i++)
-{
-scanf("%d",&num);
-}
-if(num>a)
-{
-b=a;
-a=num;
-}
-else if(num>b)
-{
-b=num;
-}
-printf("\n Second highest number=%d",b);
-}
-
-
-
 #include <stdio.h>
 
 int main()
 {
-    int temp,n;
+    int temp,n,sec,lar;
     printf(" limit:");
     scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
         printf("num:");
         scanf("%d",&temp);
-        sec=temp;
-        if(sec<temp)
+        if(i==0)
+        sec=lar=temp;
+        else if(lar<temp)
         {
-            sec=temp;
+            sec=lar;
+            lar=temp;
         }
+        else if(temp>sec)
+        sec=temp;
     }
+    printf("Largest:%d",lar);
+    printf("\nSecond largest:%d",sec);
 
     return 0;
 }
